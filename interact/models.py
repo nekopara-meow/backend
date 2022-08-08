15ed78e63749e46a4608777406d3eef5030e772b
XF <1076079235@qq.com>
2022-08-08 23:30:15 +0800

    update:更新项目、项目回收站、文件、文件回收站、个人消息、团队动态、项目动态等数据库

--- interact/models.py
+++ interact/models.py
@@ -2,6 +2,8 @@ from django.db import models
 # Create your models here.
 from django.db import models
 import datetime
+
+
 # Create your models here.
 
 
@@ -9,3 +11,30 @@ class Member_in_Team(models.Model):
     username = models.CharField(max_length=128)
     team_id = models.IntegerField(verbose_name="团队ID")
     priority = models.IntegerField(verbose_name="该成员在该项目中权限等级")
+
+
+class PersonalMessage(models.Model):
+    sender = models.CharField(max_length=128, verbose_name="发送/操作者username")
+    receiver = models.CharField(max_length=128, verbose_name="接收者username")
+    team_id = models.IntegerField(verbose_name="团队ID")
+    project_id = models.IntegerField(verbose_name="项目ID")
+    send_time = models.DateTimeField(max_length=80, verbose_name="消息发送时间", auto_now_add=True)
+    message_type = models.IntegerField(verbose_name="信息种类")
+
+class TeamMessage(models.Model):
+    team_id = models.IntegerField(verbose_name="团队ID")
+    project_id = models.IntegerField(verbose_name="项目ID")
+    sender = models.CharField(max_length=128, verbose_name="发送/操作者username")
+    receiver = models.CharField(max_length=128, verbose_name="接收者username")
+    send_time = models.DateTimeField(max_length=80, verbose_name="消息发送时间", auto_now_add=True)
+    message_type = models.IntegerField(verbose_name="信息种类")
+
+
+class ProjectMessage(models.Model):
+    team_id = models.IntegerField(verbose_name="团队ID")
+    project_id = models.IntegerField(verbose_name="项目ID")
+    file_id = models.IntegerField(verbose_name="文件ID")
+    username = models.CharField(max_length=128, verbose_name="操作者username")
+    send_time = models.DateTimeField(max_length=80, verbose_name="消息发送时间", auto_now_add=True)
+    message_type = models.IntegerField(verbose_name="信息种类")
+
--- projects/models.py
+++ projects/models.py
@@ -11,7 +11,19 @@ class Projectt(models.Model):
     project_name = models.CharField(max_length=50, verbose_name="项目名称")
     creator = models.CharField(max_length=128, verbose_name="创始人用户名")
     create_time = models.DateTimeField(max_length=80, verbose_name="项目成立时间", auto_now_add=True)
+    update_time = models.DateTimeField(max_length=80, verbose_name="项目更新时间", auto_now_add=True)
     brief_intro = models.CharField(max_length=1024, default="我不到啊")
+    file_num = models.IntegerField(verbose_name="项目内文件个数", default=0)
+    uml_num = models.IntegerField(verbose_name="项目内UML文件个数", default=0)
+    design_num = models.IntegerField(verbose_name="项目内原型设计文件个数", default=0)
+    text_num = models.IntegerField(verbose_name="项目内文档文件个数", default=0)
+    deleted = models.BooleanField(verbose_name="是否已被放入回收站", default=False)
+
+
+class ProjectBin(models.Model):
+    team_id = models.IntegerField(verbose_name="团队ID")
+    project_id = models.IntegerField(verbose_name="项目ID")
+    delete_time = models.DateTimeField(max_length=80, verbose_name="项目放入回收站时间", auto_now_add=True)
 
 
 class File(models.Model):
@@ -21,4 +33,13 @@ class File(models.Model):
     creator = models.CharField(max_length=128)
     file_id = models.AutoField(primary_key=True)
     file_url = models.CharField(max_length=1024)
+    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(verbose_name="是否已被放入回收站", default=False)
+    display = models.BooleanField(verbose_name="是否对所有人开放预览", default=False)
+
+
+class FileBin(models.Model):
+    project_id = models.IntegerField(verbose_name="项目ID")
+    file_id = models.IntegerField(verbose_name="文件ID")
+    delete_time = models.DateTimeField(max_length=80, verbose_name="文件放入回收站时间", auto_now_add=True)
