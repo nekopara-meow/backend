5350a5677ca345cad82cf0c72da8c47f3384ecec
XF <1076079235@qq.com>
2022-08-10 17:21:09 +0800

    update:更新邮件描述

--- utils/email.py
+++ utils/email.py
@@ -16,15 +16,15 @@ def make_confirm_string(user):
 def send_email_confirm(email, code):
     from django.core.mail import EmailMultiAlternatives
 
-    subject = '来自喵途论坛的注册确认邮件'
+    subject = '来自猫娘乐园的注册确认邮件'
 
-    text_content = '''感谢您的注册，这里是喵途论坛，专注于计算机系同学学习生活！\
+    text_content = '''感谢您的注册，这里是猫娘乐园，专注于团队协作与管理！\
                     如果你看到这条消息，说明你的邮箱服务器不提供HTML链接功能，请联系管理员！'''
     html_content = '''
-                       <p>感谢注册<a href="{}/#/confirm?code={}" target=blank>喵途论坛</a>，\
-                       这里是喵途论坛，专注于计算机系同学学习生活！</p>
-                       <p>请点击站点链接完成注册确认！</p>
-                       <p>此链接有效期为{}天！</p>
+                       <p>感谢注册<a href="{}/#/confirm?code={}" target=blank>猫娘乐园</a>，\
+                       相信的心就是你的魔法！</p>
+                       <p>以上链接是进入猫娘乐园的邀请函，快来加入我们，成为猫娘神教的一员吧！</p>
+                       <p>此邀请函有效期为{}天哦！</p>
                        '''.format(settings.FRONTEND, code, settings.CONFIRM_DAYS)  # url must be corrected
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [email])
     msg.attach_alternative(html_content, "text/html")
